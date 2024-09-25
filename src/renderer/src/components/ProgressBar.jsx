58aa74e3ce9c72d6384303bd93a411fbdbb54e6e
Timo Köthe <timo.info@t-online.de>
2024-09-25 18:51:35 +0200

    implement ProgressBar
    - added progress bar component including styles

---
+++ src/renderer/src/components/ProgressBar.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function ProgressBar() {
+  return (
+      <div className='progressContainer'>
+        <div className='progressBar'>
+        </div>
+      </div>
+  )
+}
+
+export default ProgressBar
--- src/renderer/src/screens/EntryView.jsx
+++ src/renderer/src/screens/EntryView.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import StartButton from '../components/StartButton'
+import ProgressBar from '../components/ProgressBar'
 
 function EntryView() {
   return (
     <>
         <h1>SmartUpdate</h1>
+        <ProgressBar />
         <StartButton />
     </>
   )
