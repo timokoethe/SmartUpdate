46f36365f8ade4efa5a6eb6efe1b8f68d26412bf
Timo Köthe <timo.info@t-online.de>
2024-09-25 15:10:48 +0200

    add font
    - added InfoView
    - added font Ropa Sans to project structure

---
+++ src/renderer/src/screens/InfoView.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ContinueButton from '../components/ContinueButton'
+
+function InfoView() {
+  return (
+    <>
+        <h1>InfoView</h1>
+        <ContinueButton />
+    </>
+  )
+}
+
+export default InfoView
