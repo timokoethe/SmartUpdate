9afdfe22748ce1b62ae4de8c55084992f4a59f3b
Timo Köthe <timo.info@t-online.de>
2024-10-03 13:28:41 +0200

    improve styling
    - added text to PickerView including the according css styles
    - added spacing css rules including margin and padding
    - added css rules for horizontal alignment
    - changed standard alignment to the top

--- src/renderer/src/screens/PickerView.jsx
+++ src/renderer/src/screens/PickerView.jsx
@@ -11,12 +11,24 @@ function PickerView() {
   const navigate = useNavigate()
   return (
     <>
-        <ProgressBar progress={3}/>
+      <ProgressBar progress={3}/>
+      <div className='spacer px-100 mt-50'>
         <InfoButton />
         <CancelButton  onClick={() => navigate('/')}/>
-        <h1 className='headline'>1. Choose Platform</h1>
-        <AndroidPicker onClick={() => navigate('/Android_Step01View')}/>
-        <ApplePicker onClick={() => navigate('/IOS_Step01View')}/>
+      </div>
+
+      <h1 className='headline'>1. Choose Platform</h1>
+      <div>
+        <p className='text mx-80 leading'>Mobile phones offer us a function to perforam updates automatically.<br/>
+          In the following we will show you how to activate this and also how to perform an update manually.
+        </p>
+        <p className='text mx-80 leading my-100'>Grab your phone and try this simultaneously on your own!</p>
+      </div>
+      <p className='text mt-100'>Please choose your platform:</p>
+      <div className='spacer mt-100 px-200'>
+        <AndroidPicker onClick={() => navigate('/Android_Step01View')} />
+        <ApplePicker onClick={() => navigate('/IOS_Step01View')} />
+      </div>
     </>
   )
 }
