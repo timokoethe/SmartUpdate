d7273fc2b4e1bab2618e134d98de62ddd7254ee8
Timo Köthe <timo.info@t-online.de>
2024-10-08 17:44:39 +0200

    improved PickerView
    - added Info Overlay to PickerView including navigation
    - added info icon to PickerView
    - added css class for centering text paragraphs

--- src/renderer/src/screens/PickerView.jsx
+++ src/renderer/src/screens/PickerView.jsx
@@ -6,14 +6,22 @@ import { useNavigate } from 'react-router-dom'
 import InfoButton from '../components/InfoButton'
 import CancelButton from '../components/CancelButton'
 import ProgressBar from '../components/ProgressBar'
+import InfoOverlay from '../components/InfoOverlay'
 
 function PickerView() {
   const navigate = useNavigate()
+  // Overlay State
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const toggleOverlay = () => setOverlayVisible(!overlayVisible);
+  
   return (
     <>
+    <div className='fullWidth'>
+      <InfoOverlay overlayNumber={1} isVisible={overlayVisible} onClose={toggleOverlay}/>
+      
       <ProgressBar progress={3}/>
       <div className='spacer px-100 mt-50' style={{alignItems: 'flex-start'}}>
-        <InfoButton />
+      <InfoButton theme={'dark'} onClick={toggleOverlay}/>
         <h1 className='headline mb-150 mt-50'>1. Choose Platform</h1>
         <CancelButton  onClick={() => navigate('/')}/>
       </div>
@@ -22,14 +30,25 @@ function PickerView() {
           In the following we will show you how to activate this and also how to perform an update manually.
         </p>
         <p className='text mx-80 leading my-100'>Grab your phone and try this simultaneously on your own!</p>
+        <p className='text center mt-100'>Please choose your platform:</p>
       </div>
-      <p className='text mt-100'>Please choose your platform:</p>
       <div className='spacer mt-100 px-200'>
         <AndroidPicker onClick={() => navigate('/Android_Step01View')} />
         <ApplePicker onClick={() => navigate('/IOS_Step01View')} />
       </div>
+    </div>
+
     </>
   )
 }
 
 export default PickerView
+
+/*
+
+
+
+
+
+
+*/
