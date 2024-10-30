bc86d0d3d2383015baf2b30ab66735fab1b8ace4
Timo Köthe <timo.info@t-online.de>
2024-10-30 13:18:41 +0100

    add Android Graphics
    - added localized android graphics for step five
    - deleted unnessecary instruction picture

--- src/renderer/src/screens/Android/Android_Step05View.jsx
+++ src/renderer/src/screens/Android/Android_Step05View.jsx
@@ -1,19 +1,29 @@
 import { useLocalization } from '../../LanguageContext'
+import { useEffect, useRef } from 'react'
 import ContinueButton from '../../components/ContinueButton'
 import { useNavigate } from 'react-router-dom'
 import InfoButton from '../../components/InfoButton'
 import CancelButton from '../../components/CancelButton'
 import BackButton from '../../components/BackButton'
 import ProgressBar from '../../components/ProgressBar'
-import image from '../../assets/phones/Android_Step05.png'
 import BottomDesign from '../../components/BottomDesign'
 import BottomFont from '../../components/BottomFont'
 import inactivityTimer from '../../components/Timer'
+import Android_DE_05 from '../../assets/phones/Android_DE_05.mp4'
+import Android_EN_05 from '../../assets/phones/Android_EN_05.mp4'
 
 function Android_Step05View() {
   const loc = useLocalization()
   inactivityTimer()
   const navigate = useNavigate()
+  const videoRef = useRef()
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = true
+      videoRef.current.play()
+    }
+  }, [])
   return (
     <>
       <ProgressBar progress={8}/>
@@ -29,7 +39,7 @@ function Android_Step05View() {
           <p className='text-medium pb-30' dangerouslySetInnerHTML={{ __html: loc.stepViewText0202 }} />
         </div>
 
-        <img src={image} alt='iPhone' className='my-100 stepImage'/>
+        <video ref={videoRef} className='phoneGraphic mt-100' src={loc.languageKey == 'de' ? Android_DE_05 : Android_EN_05 } loop />
 
         <InfoButton theme={'light'}/>
       </div>
